c81e66a45d3d018ecfc43d29e644e566224681bc
Leonan Marques <leonan.marques886@gmail.com>
2023-11-09 11:01:40 -0300

    connect credentials get method have been changed

--- dbConnection.py
+++ dbConnection.py
@@ -5,11 +5,10 @@ from dbConfig import db_config
 def get_db_connection():
     try:
         return psycopg2.connect(
-            dbname=os.environ.get('DB_NAME', db_config['dbname']),
-            user=os.environ.get('DB_USER', db_config['user']),
-            host=os.environ.get('DB_HOST', db_config['host']),
-            password=os.environ.get('DB_PASSWORD', db_config['password'])
-        )
+            dbname=db_config['dbname'],
+            user=db_config['user'],
+            host=db_config['host'],
+            password=db_config['password'])
     except Exception as e:
         print(f"Error connecting to the database: {e}")
         return None
