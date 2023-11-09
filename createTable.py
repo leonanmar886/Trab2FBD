5676e1bebe6df411ebf842cab9b86204ee40035c
Leonan Marques <leonan.marques886@gmail.com>
2023-11-09 11:52:43 -0300

    id atribute will be passed by query parameter

--- createTable.py
+++ createTable.py
@@ -8,14 +8,14 @@ def create_all_tables():
         context.execute("""
                         DROP TABLE IF EXISTS Embarcacoes CASCADE;
                         CREATE TABLE Embarcacoes (
-                            id_emb SERIAL PRIMARY KEY,
+                            id_emb PRIMARY KEY,
                             nome VARCHAR(255) NOT NULL,
                             tipo VARCHAR(255) NOT NULL
                         );
 
                         DROP TABLE IF EXISTS Tripulantes CASCADE;
                         CREATE TABLE Tripulantes (
-                            id_trp SERIAL PRIMARY KEY,
+                            id_trp PRIMARY KEY,
                             nome VARCHAR(255) NOT NULL,
                             data_nasc DATE NOT NULL,
                             funcao VARCHAR(255) NOT NULL,
@@ -24,7 +24,7 @@ def create_all_tables():
 
                         DROP TABLE IF EXISTS Empregados CASCADE;
                         CREATE TABLE Empregados (
-                            id_emp SERIAL PRIMARY KEY,
+                            id_emp PRIMARY KEY,
                             nome VARCHAR(255) NOT NULL,
                             data_nasc DATE NOT NULL,
                             funcao VARCHAR(255) NOT NULL
@@ -32,7 +32,7 @@ def create_all_tables():
 
                         DROP TABLE IF EXISTS Movimentacoes CASCADE;
                         CREATE TABLE Movimentacoes (
-                            id_mov SERIAL PRIMARY KEY,
+                            id_mov PRIMARY KEY,
                             data DATE NOT NULL,
                             tipo VARCHAR(255) NOT NULL,
                             id_emb INTEGER REFERENCES Embarcacoes
