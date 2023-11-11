006656ee5016b1ee30ce49a336a5de31d32dce5d
Leonan Marques <leonan.marques886@gmail.com>
2023-11-10 21:40:58 -0300

    arquivo main foi criado

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import createTable
 import insertItems
 import selectItems
 
+from dbConfig import conn, context
+
+createTable.createTable(conn, context)
+insertItems.insert_items(conn, context)
+selectItems.select_boats_and_crew(context)
+selectItems.select_employes(context)
+selectItems.select_boats(context)
