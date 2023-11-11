0bd119803966dd2a2cc8cc8e8dca7bb0b741a0a1
Leonan Marques <leonan.marques886@gmail.com>
2023-11-10 22:10:06 -0300

    select items functions have been updated

--- selectItems.py
+++ selectItems.py
@@ -21,13 +21,12 @@ Utilizamos a cláusula LEFT JOIN para incluir todas as embarcações, mesmo aque
 
 def select_employes(context):
     context.execute("""
-        SELECT * AS dadosEmpregados
-        FROM Empregados e
+        SELECT * FROM Empregados e
         WHERE e.id_emp IN (SELECT me.id_emp FROM Movimentacoes_Empregados me INNER JOIN Movimentacoes m ON m.id_mov=me.id_mov WHERE m.id_mov = 1 )
     """)
     result1 = context.fetchall()
 
-    table = PrettyTable(['dadosEmpregados'])
+    table = PrettyTable(['id_emp', 'nome', 'data_nasc', 'funcao'])
     for row in result1:
         table.add_row(row)
 
@@ -35,16 +34,15 @@ def select_employes(context):
 
 def select_boats(context):
     context.execute("""
-            SELECT COUNT(m.id_emb) AS quantEmbarcacoes
-            FROM Embarcacoes e INNER JOIN Movimentacao m
+            SELECT COUNT(m.id_emb) AS qtdEmbarcacoes
+            FROM Embarcacoes e 
+            INNER JOIN Movimentacoes m
             ON m.id_emb = e.id_emb WHERE e.tipo = 'Cargueiro'
         """)
     result2 = context.fetchall()
 
-    table = PrettyTable(['quantEmbarcacoes'])
+    table = PrettyTable(['qtdEmbarcacoes'])
     for row in result2:
         table.add_row(row)
 
     print(table)
-
-    context.close()
