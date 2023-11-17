7b6806d3794a72dc46461782aeb75002b83b0842
Leonan Marques <leonan.marques886@gmail.com>
2023-11-16 21:31:40 -0300

    salvar ante s da merda
    Co-authored-by: Levy Oliveira <levy-oliveira@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,28 +5,40 @@ import transactions
 import storedProcedures
 import triggers
 
-#2.2 Criação de Tabelas
-createTable.createTable()
+X = input("""
+  Digite uma opção: \n
+  1. Criar Tabelas \n
+  2. Inserir Dados \n
+  3. Consultar \n
+  4. Transações \n
+  5. Procedimentos Armazenados \n
+  6. Gatilhos \n
+""")
 
-#2.3 Inserção de Dados
-insertItems.insert_items()
-
-# 2.4 Consulta
-selectItems.select_boats_and_crew()
-selectItems.select_employes()
-selectItems.select_boats()
-
-# 2.5 Transações
-transactions.insert_move()
-transactions.insert_move_employe()
-transactions.select_qtd_moves()
-
-#2.6 Procedimentos Armazenados
-storedProcedures.employer_of_month()
-storedProcedures.execute_employer_of_month('2023-10-21')
-
-#2.7 Gatilhos
-triggers.create_capitain_trigger()
-triggers.create_move_trigger()
-triggers.test_capitan_trigger()
-triggers.test_move_trigger()
+match X:
+  case "1":
+    createTable.createTable()
+ 
+  case "2":
+    insertItems.insert_items()
+    
+  case "3":
+    selectItems.select_boats_and_crew()
+    selectItems.select_employes()
+    selectItems.select_boats()
+   
+  case "4":
+    transactions.insert_move()
+    transactions.insert_move_employe()
+    transactions.select_qtd_moves()
+   
+  case "5":
+    storedProcedures.employer_of_month()
+    storedProcedures.execute_employer_of_month('2023-10-21')
+  
+  case "6":
+    triggers.create_capitain_trigger()
+    triggers.create_move_trigger()
+    triggers.test_capitan_trigger()
+    triggers.test_move_trigger()
+  
